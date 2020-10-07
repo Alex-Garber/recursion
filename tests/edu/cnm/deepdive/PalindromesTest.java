bfac14ba8a1110e323fc41756f4c8ba11065020f
Alex Garber <alexgarber6@gmail.com>
2020-10-07 16:08:42 -0600

    Wrote test for testRecursive method

---
+++ tests/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,34 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PalindromesTest {
+  static final String[] inputValues = {
+     "radar",
+     "sonar",
+      "abba",
+      "abb",
+      "x",
+      ""
+  };
+  static final boolean[] expectedValues = {
+      true,
+      false,
+      true,
+      false,
+      true,
+      true
+  };
+
+  @Test
+  void computeRecursive(){
+    for (int i = 0; i < inputValues.length; i++){
+      String input = inputValues[i];
+      boolean expected = expectedValues[i];
+      boolean actual = Palindromes.testRecursive(input);
+      assertEquals(expected, actual);
+  }
+  }
+}
